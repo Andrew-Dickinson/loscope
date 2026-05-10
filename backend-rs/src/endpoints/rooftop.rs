77f5ee810781bb6965cf56a0d42fbdc9ca6f59d6
Andrew Dickinson <andrew.dickinson.0216@gmail.com>
2026-05-09 23:38:38 -0400

    Implement rooftop OBJ render endpoint

--- backend-rs/src/endpoints/rooftop.rs
+++ backend-rs/src/endpoints/rooftop.rs
@@ -1,30 +1,37 @@
-use rocket::http::Status;
-use rocket::State;
+use std::pin::Pin;
+use std::str::FromStr;
+use futures_util::{Stream, StreamExt};
+use rocket::http::{ContentType, MediaType, Status};
+use rocket::response::stream::TextStream;
+use rocket::{Response, State};
 use wkt::ToWkt;
-use crate::building::heightmap::BINId;
+use crate::building::heightmap::{get_intersecting_tiles, BINId, RooftopHeightMapFactory};
 use crate::providers::Providers;
 use crate::types::coords::{NYSCoords3};
 use crate::types::errors::AssetErr;
-
-#[derive(Responder)]
-#[response(status = 200, content_type = "model/obj")]
-pub struct ObjModel(Vec<u8>);
-
+use rocket::response::Debug;
 
 #[get("/render/<bin_id>")]
-pub async fn render_rooftop(
+pub async fn render_rooftop<'a>(
     bin_id: &str,
     providers: &State<Providers>
-) -> Result<String, Status> {
-// ) -> Result<ObjModel, Status> {
+) -> Result<(ContentType, TextStream![String]), Status>  {
     // TODO: Online lookup to validate it's a real BIN?
     let Ok(bin_id) = BINId::parse(bin_id) else { return Err(Status::BadRequest) };
 
-    let footprint = providers.footprint_provider().get_footprint(&bin_id).await?;
+    let factory = RooftopHeightMapFactory::new(
+        providers.footprint_provider().as_ref(),
+        providers.elevation_tile_provider().as_ref()
+    );
+    let heightmap = factory.create(bin_id).await
+        .map_err(|e| {eprintln!("{:?}", e); e})?;
 
+    let obj_stream = TextStream! {
+        let mut stream = std::pin::pin!(heightmap.to_rooftop_obj_stream());
+        while let Some(chunk) = stream.next().await {
+            yield chunk;
+        }
+    };
 
-    // TODO: Impelmetn me
-    // https://docs.rs/wkt/latest/wkt/ ?
-
-    Ok(footprint.wkt_string())
+    Ok((ContentType::new("model", "obj"), obj_stream))
 }
