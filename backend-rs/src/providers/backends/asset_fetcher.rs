6913203b6b92c02e493fe9a2f7cf9ee65eb2a559
Andrew Dickinson <andrew.dickinson.0216@gmail.com>
2026-05-23 12:32:44 -0400

    Fix: race condition on fs access in asset fetcher

--- backend-rs/src/providers/backends/asset_fetcher.rs
+++ backend-rs/src/providers/backends/asset_fetcher.rs
@@ -5,6 +5,7 @@ use std::path::{Path, PathBuf};
 use aws_sdk_s3::error::SdkError;
 use aws_sdk_s3::operation::get_object::GetObjectError;
 use derive_new::new;
+use rand::Rng;
 use typed_path::{Utf8UnixPath, Utf8UnixPathBuf};
 use crate::types::errors::AssetErr;
 use strum_macros::{AsRefStr, Display};
@@ -49,7 +50,15 @@ impl AssetFetcher for S3AssetFetcher {
 
         let local_path_temp_owned = {
             let mut local_path_temp_mut = local_path.to_path_buf().into_os_string();
-            local_path_temp_mut.push(".tmp");
+            // Use a temp file with a random name to reduce the chance of a collision with another
+            // thread that's downloading the same asset. 100k is relatively low, but the only
+            // consequence of a collision is that an AssetErr will be thrown below when the
+            // second thread tries to write it at the same time
+            // TODO: A proper mutex accounting system keyed by local_path could probably resolve
+            //  this for real
+            local_path_temp_mut.push(
+                format!("-{:05}.tmp", rand::thread_rng().gen_range(1..100_000))
+            );
             PathBuf::from(local_path_temp_mut)
         };
         let local_path_temp = local_path_temp_owned.as_path();
