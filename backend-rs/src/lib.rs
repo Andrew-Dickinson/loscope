a242e607135610d16dca07e1b5923da08067fddb
Andrew Dickinson <andrew.dickinson.0216@gmail.com>
2026-05-22 21:39:17 -0400

    Extract into lib.rs

---
+++ backend-rs/src/lib.rs
@@ -0,0 +1,9 @@
+#[macro_use] extern crate rocket;
+
+pub mod types;
+pub mod providers;
+pub mod util;
+pub mod endpoints;
+pub mod building;
+pub mod sample_points;
+pub mod analysis;
--- backend-rs/src/main.rs
+++ backend-rs/src/main.rs
@@ -1,32 +1,21 @@
-mod types;
-pub mod providers;
-pub mod util;
-pub mod endpoints;
-pub mod building;
-pub mod sample_points;
-pub mod analysis;
+use fresnel_2::endpoints::coords::gps_to_nys;
+use fresnel_2::endpoints::rooftop::{render_rooftop, sample_points as sample_points_endpoint};
+use fresnel_2::endpoints::tileview::get_terrain_ortho;
+use fresnel_2::providers::Providers;
+use fresnel_2::util::coord_conversion::{init_coord_converter_factory, CoordinateConverter};
 
-#[macro_use] extern crate rocket;
-
-use crate::endpoints::rooftop::sample_points as sample_points_endpoint;
-use crate::endpoints::coords::gps_to_nys;
-use crate::endpoints::rooftop::render_rooftop;
-use crate::endpoints::tileview::get_terrain_ortho;
-use crate::providers::Providers;
-use crate::util::coord_conversion::{init_coord_converter_factory, CoordinateConverter};
-
-#[get("/healthcheck")]
+#[rocket::get("/healthcheck")]
 fn health_check() -> &'static str {
     "Healthy"
 }
 
-#[launch]
+#[rocket::launch]
 async fn rocket() -> _ {
     init_coord_converter_factory(CoordinateConverter::new);
     rocket::build()
         .manage(Providers::new_from_env().await.unwrap())
-        .mount("/api", routes![health_check])
-        .mount("/api/rooftop", routes![render_rooftop,sample_points_endpoint])
-        .mount("/api/tileview", routes![get_terrain_ortho])
-        .mount("/api/coords", routes![gps_to_nys])
+        .mount("/api", rocket::routes![health_check])
+        .mount("/api/rooftop", rocket::routes![render_rooftop, sample_points_endpoint])
+        .mount("/api/tileview", rocket::routes![get_terrain_ortho])
+        .mount("/api/coords", rocket::routes![gps_to_nys])
 }
