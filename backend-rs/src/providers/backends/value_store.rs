d37dff5f2574ff18d0453a21d8b4964dd12ce735
Andrew Dickinson <andrew.dickinson.0216@gmail.com>
2026-05-23 15:10:15 -0400

    Add tests to InMemoryValueStore

--- backend-rs/src/providers/backends/value_store.rs
+++ backend-rs/src/providers/backends/value_store.rs
@@ -22,6 +22,50 @@ impl InMemoryValueStore {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn get_after_put_returns_value() {
+        let store = InMemoryValueStore::new();
+        store.put("k".into(), b"hello".to_vec()).unwrap();
+        assert_eq!(store.get("k".into()).unwrap(), b"hello");
+    }
+
+    #[test]
+    fn get_missing_key_returns_not_found() {
+        let store = InMemoryValueStore::new();
+        assert!(matches!(store.get("missing".into()), Err(AssetErr::AssetNotFound(_))));
+    }
+
+    #[test]
+    fn put_overwrites_existing_key() {
+        let store = InMemoryValueStore::new();
+        store.put("k".into(), b"first".to_vec()).unwrap();
+        store.put("k".into(), b"second".to_vec()).unwrap();
+        assert_eq!(store.get("k".into()).unwrap(), b"second");
+    }
+
+    #[test]
+    fn keys_are_independent() {
+        let store = InMemoryValueStore::new();
+        store.put("a".into(), b"1".to_vec()).unwrap();
+        store.put("b".into(), b"2".to_vec()).unwrap();
+        assert_eq!(store.get("a".into()).unwrap(), b"1");
+        assert_eq!(store.get("b".into()).unwrap(), b"2");
+    }
+
+    #[test]
+    fn get_returns_cloned_value() {
+        let store = InMemoryValueStore::new();
+        store.put("k".into(), b"data".to_vec()).unwrap();
+        let v1 = store.get("k".into()).unwrap();
+        let v2 = store.get("k".into()).unwrap();
+        assert_eq!(v1, v2);
+    }
+}
+
 impl ValueStore for InMemoryValueStore {
     fn put(&self, key: String, value: Vec<u8>) -> Result<(), AssetErr> {
         self.map.lock()
