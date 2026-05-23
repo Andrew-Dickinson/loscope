31751ff98c90952cad51bfcb7ec8b34c8c2f90e5
Andrew Dickinson <andrew.dickinson.0216@gmail.com>
2026-05-23 15:10:58 -0400

    Add /api/analysis/overview/<uuid> endpoint

--- backend-rs/src/endpoints/analysis.rs
+++ backend-rs/src/endpoints/analysis.rs
@@ -1,6 +1,8 @@
 use rocket::http::Status;
 use rocket::serde::json::Json;
 use rocket::State;
+use uuid::Uuid;
+use crate::analysis::map_overview::PointEvaluationOverview;
 use crate::analysis::point_evaluation::{evaluate_points, valid_analysis_frequency, PointEvaluationInput, PointEvaluationOutput};
 use crate::providers::Providers;
 
@@ -19,10 +21,25 @@ pub async fn point_analysis(
     let result = evaluate_points(
         point_pair.into_inner(),
         providers.elevation_tile_provider().as_ref()
-    ).await?;
-    
+    ).await
+        //.map_err(|err| { eprintln!("{:?}", err); err })
+    ?;
+
     providers.point_eval_result_provider().put(&result)
         .or_else(|_| Err(Status::InternalServerError))?;
 
     Ok(Json(result.into_output()))
 }
+
+#[get("/overview/<analysis_id>")]
+pub async fn map_overview(
+    analysis_id: &str,
+    providers: &State<Providers>
+) -> Result<Json<PointEvaluationOverview>, Status> {
+    let analysis_id = Uuid::parse_str(analysis_id).map_err(|_| Status::BadRequest)?;
+    let analysis_outcome = providers.point_eval_result_provider().get(&analysis_id)
+        // .map_err(|err| { eprintln!("{:?}", err); err })
+        ?;
+    Ok(Json((&analysis_outcome).into()))
+}
+
--- backend-rs/src/main.rs
+++ backend-rs/src/main.rs
@@ -1,4 +1,4 @@
-use fresnel_2::endpoints::analysis::point_analysis;
+use fresnel_2::endpoints::analysis::{map_overview, point_analysis};
 use fresnel_2::endpoints::coords::gps_to_nys;
 use fresnel_2::endpoints::rooftop::{render_rooftop, sample_points as sample_points_endpoint};
 use fresnel_2::endpoints::tileview::get_terrain_ortho;
@@ -19,5 +19,5 @@ async fn rocket() -> _ {
         .mount("/api/rooftop", rocket::routes![render_rooftop, sample_points_endpoint])
         .mount("/api/tileview", rocket::routes![get_terrain_ortho])
         .mount("/api/coords", rocket::routes![gps_to_nys])
-        .mount("/api/analysis", rocket::routes![point_analysis])
+        .mount("/api/analysis", rocket::routes![point_analysis,map_overview])
 }
