c33b8ae4148840a132eb8ae54239767e987a3df6
Andrew Dickinson <andrew.dickinson.0216@gmail.com>
2026-05-24 10:54:40 -0400

    Add tileview endpoints

--- backend-rs/src/main.rs
+++ backend-rs/src/main.rs
@@ -2,7 +2,7 @@ use fresnel_2::endpoints::analysis::{fresnel_kml, intersection_visualization, ma
 use fresnel_2::endpoints::coords::gps_to_nys;
 use fresnel_2::endpoints::meshdb::resolve_number;
 use fresnel_2::endpoints::rooftop::{render_rooftop, sample_points as sample_points_endpoint};
-use fresnel_2::endpoints::tileview::get_terrain_ortho;
+use fresnel_2::endpoints::tileview::{get_fresnel_slice_obj, get_terrain_obstruction_meta, get_terrain_obstruction_obj, get_terrain_ortho, get_terrain_raster, get_terrain_tile_overview};
 use fresnel_2::providers::Providers;
 use fresnel_2::util::coord_conversion::{init_coord_converter_factory, CoordinateConverter};
 
@@ -18,7 +18,7 @@ async fn rocket() -> _ {
         .manage(Providers::new_from_env().await.unwrap())
         .mount("/api", rocket::routes![health_check])
         .mount("/api/rooftop", rocket::routes![render_rooftop, sample_points_endpoint])
-        .mount("/api/tileview", rocket::routes![get_terrain_ortho])
+        .mount("/api/tileview", rocket::routes![get_terrain_tile_overview,get_terrain_raster,get_terrain_obstruction_meta,get_terrain_obstruction_obj,get_fresnel_slice_obj,get_terrain_ortho])
         .mount("/api/coords", rocket::routes![gps_to_nys])
         .mount("/api/analysis", rocket::routes![point_analysis,map_overview,intersection_visualization,fresnel_kml])
         .mount("/api/meshdb", rocket::routes![resolve_number])
